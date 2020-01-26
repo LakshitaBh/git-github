521efa6e2d42978086da3cf4814d478422c57874
Lakshita Bhargava <lakshitabhargava96@gmail.com>
2020-01-26 22:28:18 +0530

    testing curl

--- test.py
+++ test.py
@@ -4,5 +4,6 @@ Created on Sun Jan 26 21:38:47 2020
 
 @author: bharg
 """
-
+import test2
 print("hello")
+print(test2.a)
---
+++ test2.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 26 22:27:19 2020
+
+@author: bharg
+"""
+
+a = 1
